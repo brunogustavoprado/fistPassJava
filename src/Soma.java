dc456535739773a04a813e86d7932a517152cd25
kn1zone <brgustavo648@gmail.com>
2024-05-09 09:42:56 -0400

    First Java Aplication

---
+++ src/Soma.java
@@ -0,0 +1,7 @@
+public class Soma {
+    public static void main(String[] args) {
+        System.out.println("Esse é o resultado de uma soma 5+10:");
+        System.out.println(5+10);
+    }
+}
+
---
+++ src/subtracao.java
@@ -0,0 +1,6 @@
+public class subtracao {
+    public static void main(String[] args) {
+        System.out.println("Esse é o resultado de uma subtração de 5-10:");
+        System.out.println(5-10);
+    }
+}
